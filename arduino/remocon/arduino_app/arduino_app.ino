227bae511cdf6ab2ca4249a52a55d4bb1ede3252
riversnails <ekgur120736@gmail.com>
2020-09-14 15:00:01 +0900

    arduino remocon & ultrasonic

---
+++ arduino/remocon/arduino_app/arduino_app.ino
@@ -0,0 +1,43 @@
+#include "remocon.h"
+#include "ultrasonic.h"
+#include "exti.h"
+
+#define REMOCON_PIN 2
+#define ULTRASONIC_ECHO_PIN 3
+#define ULTRASONIC_TRIGGER_PIN 4
+
+float ultrasonic_dist = 0;
+
+char remocon_number = 0;
+unsigned long c_millis = 0;
+unsigned long p_millis_ultra = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  remocon_init(REMOCON_PIN); // arduino pin 2
+  ultrasonic_init(ULTRASONIC_ECHO_PIN, ULTRASONIC_TRIGGER_PIN); // arduino pin 3
+}
+
+void loop()
+{
+  c_millis = millis();
+  if (c_millis - p_millis_ultra > 1000)
+  {
+    p_millis_ultra = c_millis;
+    ultrasonic_trigger();
+  }
+
+  if (ultrasonic_callback(&ultrasonic_dist))
+  {
+    ultrasonic_callback_flag_clear();
+    Serial.println(ultrasonic_dist);
+  }
+
+  if (remocon_callback(&remocon_number))
+  {
+    int_callback_flag_clear();
+    Serial.println(remocon_number);
+  }
+
+}
