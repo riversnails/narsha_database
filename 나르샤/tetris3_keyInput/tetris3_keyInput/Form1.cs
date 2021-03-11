6bcffc02d353048c45313b32b1c5b08aefd6fe6c
riversnails <ekgur120736@gmail.com>
2021-03-11 16:49:49 +0900

    rebuild

---
+++ 나르샤/tetris3_keyInput/tetris3_keyInput/Form1.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tetris3_keyInput
+{
+    public partial class Form1 : Form
+    {
+        static System.Windows.Forms.Timer timer_1sec = new System.Windows.Forms.Timer();
+
+        int x = 5, y = 0;
+        int offset_x = 10, offset_y = 10;
+
+        Random rnd = new Random();
+
+        int current_block = 0; // 블록종류
+        int next_block;
+
+        Color[] block_color = new Color[8]
+        {
+            Color.White,
+            Color.Red,
+            Color.Orange,
+            Color.Gold,
+            Color.Green,
+            Color.Blue,
+            Color.Purple,
+            Color.Black
+        }; // 색갈 배열
+
+        int[,,] block_n; // 블록7개 선언
+
+        PictureBox nextBlockBox = new PictureBox(); // 다음블록
+        PictureBox[,] pictureBox = new PictureBox[20, 10]; // Background
+        int[,] block_state = new int[20, 10]; // Current block
+        Color[,] saved_color = new Color[20, 10]; //Block color save
+
+        private void initBlock() // 블록 로테이트된거 초기화 && 블록 초기화 값 저장해놓은거
+        {
+            block_n = new int[7, 4, 4]
+            {
+                {
+                    {0, 0, 0, 0},
+                    {0, 1, 0, 0},
+                    {0, 1, 1, 1},
+                    {0, 0, 0, 0},
+                },
+                {
+                    {0, 0, 0, 0},
+                    {0, 1, 1, 0},
+                    {0, 1, 1, 0},
+                    {0, 0, 0, 0},
+                },
+                {
+                    {0, 0, 1, 0},
+                    {0, 0, 1, 0},
+                    {0, 0, 1, 0},
+                    {0, 0, 1, 0},
+                },
+                {
+                    {0, 0, 0, 0},
+                    {0, 1, 0, 0},
+                    {0, 1, 1, 0},
+                    {0, 0, 1, 0},
+                },
+                {
+                    {0, 0, 0, 0},
+                    {0, 0, 1, 0},
+                    {0, 1, 1, 0},
+                    {0, 1, 0, 0},
+                },
+                {
+                    {0, 0, 0, 0},
+                    {0, 1, 1, 0},
+                    {0, 0, 1, 0},
+                    {0, 0, 1, 0},
+                },
+                {
+                    {0, 0, 0, 0},
+                    {0, 0, 1, 0},
+                    {0, 1, 1, 0},
+                    {0, 0, 1, 0},
+                }
+            };
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            next_block = rnd.Next(7);
+
+            initBlock();
+
+            timer_1sec.Interval = 1000;
+            timer_1sec.Tick += new EventHandler(event_1sec);
+            timer_1sec.Start();
+
+            this.KeyPreview = true;
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int i, j;
+
+            for (j = 0; j < 20; j++)
+            {
+                for (i = 0; i < 10; i++)
+                {
+                    pictureBox[j, i] = new PictureBox();
+                    pictureBox[j, i].BackColor = block_color[0];
+                    pictureBox[j, i].Location = new Point(offset_x + (i * 22), offset_y + (j * 22));
+                    pictureBox[j, i].Size = new Size(20, 20);
+                    saved_color[j, i] = Color.White;
+                    this.Controls.Add(pictureBox[j, i]);
+                }
+            }
+
+            offset_x = 250;
+            offset_y = 10;
+
+            nextBlockBox.BackColor = Color.Gray;
+            nextBlockBox.Location = new Point(offset_x, offset_y);
+            nextBlockBox.Size = new Size(90, 90);
+            Controls.Add(nextBlockBox);
+            drawNextBlock();
+        }
+
+        private void drawNextBlock()
+        {
+            Bitmap map = new Bitmap(90, 90);
+            int x = 2, y = 2;
+
+            for(int j = 0; j < 4; j++)
+            {
+                for(int i = 0; i < 4; i++)
+                {
+                    if(block_n[next_block,j,i] == 1)
+                    {
+                        drawBlock(x, y, 20, map, block_color[next_block + 1]);
+                        
+                    }
+                    else
+                    {
+                        drawBlock(x, y, 20, map, Color.White);
+                    }
+                    x += 22;
+                }
+                x = 2;
+                y += 22;
+            }
+            nextBlockBox.Image = map;
+        }
+
+        private void drawBlock(int x, int y, int size, Bitmap map, Color color)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    map.SetPixel(x + i, y + j, color);
+                }
+            }
+        }
+
+        private bool overlap_check(int x, int y)
+        {
+            return overlap_check(x, y, false);
+        }
+        private bool overlap_check(int x, int y, bool is_rotate)
+        {
+            int[,] arr = new int[4, 4];
+            for (int j = 0; j < 4; j++) for (int i = 0; i < 4; i++) arr[j, i] = block_n[current_block, j, i];
+
+            if (is_rotate) arr = rotateBlockL();
+
+            for (int j = 0; j < 4; j++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (arr[j, i] == 1)
+                    {
+                        if (x + i >= 10 || x + i < 0) return true;
+                        if (y + j >= 20 || y + j < 0) return true;
+
+                        if (block_state[j + y, i + x] != 0) return true;
+
+                    }
+                }
+            }
+            return false;
+        }
+
+        private void draw_block_l(int x, int y)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (block_n[current_block, j, i] == 1)
+                    {
+                        pictureBox[j + this.y, i + this.x].BackColor = block_color[0]; // Clear block
+                    }
+                }
+            }
+            this.y += y;
+            this.x += x;
+
+            for (int j = 0; j < 4; j++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (block_n[current_block, j, i] == 1)
+                    {
+                        pictureBox[j + this.y, i + this.x].BackColor = block_color[current_block + 1]; // Draw block
+                    }
+                }
+            }
+
+        }
+
+        private void resetBlock()
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    pictureBox[j, i].BackColor = saved_color[j, i];
+                }
+            }
+        }
+
+        private int[,] rotateBlockL()
+        {
+            int[,] temp = new int[4, 4];
+            for (int j = 0; j < 4; j++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    temp[j, i] = block_n[current_block, 3 - i, j];
+                }
+            }
+            return temp;
+        }
+
+        private void event_1sec(Object myObject, EventArgs myEventArgs)
+        {
+            if (!overlap_check(x, y + 1))
+                draw_block_l(0, 1);
+            else // 땅바닥에 쿵
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        if (block_n[current_block, j, i] == 1)
+                        {
+                            block_state[j + y, i + x] = 1;
+                            saved_color[j + y, i + x] = block_color[current_block + 1];
+                        }
+                    }
+                }
+
+                //------------------------------------
+                for (int j = 0; j < 20; j++)
+                {
+                    int count = 0;
+                    for (int i = 0; i < 10; i++)
+                    {
+                        if (block_state[j, i] == 1) count++;
+                    }
+
+                    if (count == 10)
+                    {
+                        for (int level = j; level > 0; level--)
+                        {
+                            for (int i = 0; i < 10; i++)
+                            {
+                                block_state[level, i] = block_state[level - 1, i];
+                                saved_color[level, i] = saved_color[level - 1, i];
+                            }
+                        }
+                        j--;
+                    }
+                }
+
+                //------------------------------------
+                current_block = next_block; // 다음블록을 현재 블록으로
+                next_block = rnd.Next(7); // 다음블록을 랜덤으로 지정
+                drawNextBlock(); // 
+
+                initBlock();
+                resetBlock();
+                x = 4;
+                y = 1;
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 'a')
+            {
+                if (!overlap_check(x - 1, y))
+                    draw_block_l(-1, 0);
+            }
+            else if (e.KeyChar == 'd')
+            {
+                if (!overlap_check(x + 1, y))
+                    draw_block_l(1, 0);
+            }
+            else if (e.KeyChar == 'w')
+            {
+                if (!overlap_check(x, y - 1))
+                    draw_block_l(0, -1);
+            }
+            else if (e.KeyChar == 's')
+            {
+                if (!overlap_check(x, y + 1))
+                    draw_block_l(0, 1);
+            }
+            else if (e.KeyChar == 'r')
+            {
+                if (!overlap_check(x, y, true))
+                {
+                    for (int j = 0; j < 4; j++)
+                        for (int i = 0; i < 4; i++)
+                            if (block_n[current_block, j, i] == 1)
+                                pictureBox[y + j, x + i].BackColor = Color.White;
+
+                    int[,] arr = rotateBlockL();
+                    for (int j = 0; j < 4; j++) for (int i = 0; i < 4; i++) block_n[current_block, j, i] = arr[j, i];
+
+                    for (int j = 0; j < 4; j++)
+                        for (int i = 0; i < 4; i++)
+                            if (block_n[current_block, j, i] == 1)
+                                pictureBox[y + j, x + i].BackColor = block_color[current_block + 1];
+
+                }
+            }
+        }
+    }
+}
---
+++ 나르샤/tetris_console/tetris_console/Program.cs
@@ -0,0 +1,376 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace tetris_console // 전체를 묶어주는 범위
+//{
+//    class Program
+//    {
+//        static void Main(string[] args) // 메모리 구조 | text data stack heep |    static = data영역에 남아있어서 exe가 끝날때 까지 존재
+//        {
+
+//        }
+//    }
+//}
+
+//// new 키워드 사용시 heep 영역에 올라감
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static int[,,,] block_L = new int[7, 4, 4, 4] {
+            {
+                { {0,0,0,0},{0,1,0,0},{0,1,1,1},{0,0,0,0} }, // ㄴ
+                { {0,0,0,0},{0,1,1,0},{0,1,0,0},{0,1,0,0} },
+                { {0,0,0,0},{1,1,1,0},{0,0,1,0},{0,0,0,0} },
+                { {0,0,1,0},{0,0,1,0},{0,1,1,0},{0,0,0,0} }
+            },
+            {
+                { {0,0,0,0},{0,0,1,0},{1,1,1,0},{0,0,0,0} }, // 역ㄴ
+                { {0,1,0,0},{0,1,0,0},{0,1,1,0},{0,0,0,0} },
+                { {0,0,0,0},{0,1,1,1},{0,1,0,0},{0,0,0,0} },
+                { {0,0,0,0},{0,1,1,0},{0,0,1,0},{0,0,1,0} }
+            },
+            {
+                { {0,0,0,0},{0,1,1,0},{0,1,1,0},{0,0,0,0} }, // ㅁ
+                { {0,0,0,0},{0,1,1,0},{0,1,1,0},{0,0,0,0} },
+                { {0,0,0,0},{0,1,1,0},{0,1,1,0},{0,0,0,0} },
+                { {0,0,0,0},{0,1,1,0},{0,1,1,0},{0,0,0,0} }
+            },
+            {
+                { {0,0,0,0},{0,0,1,1},{0,1,1,0},{0,0,0,0} }, // 역z
+                { {0,0,0,0},{0,1,0,0},{0,1,1,0},{0,0,1,0} },
+                { {0,0,0,0},{0,0,1,1},{0,1,1,0},{0,0,0,0} },
+                { {0,0,0,0},{0,1,0,0},{0,1,1,0},{0,0,1,0} }
+            },
+            {
+                { {0,0,0,0},{1,1,0,0},{0,1,1,0},{0,0,0,0} }, // z
+                { {0,0,1,0},{0,1,1,0},{0,1,0,0},{0,0,0,0} },
+                { {0,0,0,0},{1,1,0,0},{0,1,1,0},{0,0,0,0} },
+                { {0,0,1,0},{0,1,1,0},{0,1,0,0},{0,0,0,0} }
+            },
+            {
+                { {0,0,0,0},{0,1,0,0},{1,1,1,0},{0,0,0,0} }, // ㅗ
+                { {0,1,0,0},{0,1,1,0},{0,1,0,0},{0,0,0,0} },
+                { {0,0,0,0},{0,1,1,1},{0,0,1,0},{0,0,0,0} },
+                { {0,0,0,0},{0,0,1,0},{0,1,1,0},{0,0,1,0} }
+            },
+            {
+                { {0,0,0,0},{1,1,1,1},{0,0,0,0},{0,0,0,0} }, // |
+                { {0,1,0,0},{0,1,0,0},{0,1,0,0},{0,1,0,0} },
+                { {0,0,0,0},{1,1,1,1},{0,0,0,0},{0,0,0,0} },
+                { {0,1,0,0},{0,1,0,0},{0,1,0,0},{0,1,0,0} },
+            }
+        };
+
+
+        public static int[,] background = new int[12, 12]
+        {
+            {1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,0,0,0,0,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1}
+        };
+
+
+        public static Random rand = new Random();
+        public static int overlap_count = 0;
+        public static int line_count = 0;
+        public static int rotate_num = 0;
+        public static char space = ' ';
+        public static int now_block = rand.Next(0,6);
+        public static int next_block = rand.Next(0,6);
+
+
+        public static void delete_block(int x, int y, int block)
+        {
+            for (int j = 0; j < 4; j++) // 지우기
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (block_L[block, rotate_num , j, i] == 1)
+                    {
+                        Console.SetCursorPosition(x + i, y + j);
+                        Console.Write(space);
+                    }
+                }
+                Console.WriteLine("");
+
+            }
+        }
+
+        public static void make_block(int x, int y, int block)
+        {
+            for (int j = 0; j < 4; j++) // ㄴ자 블록 그리기 코드
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (block_L[block, rotate_num, j, i] == 1)
+                    {
+                        Console.SetCursorPosition(x + i, y + j);
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine("");
+
+            }
+        }
+
+
+        public static int overlap_check(int x, int y, int rotate_num)
+        {
+            int overlap_count = 0;
+
+            for (int j = 0; j < 4; j++)
+            {
+                for(int i = 0; i < 4; i ++)
+                {
+                    if (j + y < 12)
+                    {
+                        if (block_L[now_block, rotate_num, j, i] == 1 && background[j + y, i + x] == 1)
+                        {
+                            overlap_count++;
+                        }
+                    }
+                }
+            }
+            return overlap_count;
+            
+        }
+
+        public static void draw_background(int x, int y)
+        {
+            for (int j = 0; j < 12; j++) // 백그라운드 출력
+            {
+                for (int i = 0; i < 12; i++)
+                {
+                    if (background[j, i] == 1)
+                    {
+                        Console.SetCursorPosition(i, j);
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(i, j);
+                        Console.Write(space);
+                    }
+                }
+                Console.WriteLine("");
+            }
+
+            for (int j = 0; j < 12; j++) // 백그라운드 디버깅용 출력
+            {
+                for (int i = 0; i < 12; i++)
+                {
+                    if (background[j, i] == 1)
+                    {
+
+                        Console.SetCursorPosition(i + 25, j);
+                        Console.Write(background[j, i]);
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(i + 25, j);
+                        Console.Write(background[j, i]);
+                    }
+                }
+                Console.WriteLine("");
+            }
+            
+        }
+        
+        public static void insert_block(int x, int y)
+        {
+            for (int j = 0; j < 4; j++) // 백그라운드에 블록 넣어버리기
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (block_L[now_block, rotate_num, j, i] == 1)
+                    {
+                        background[j + y, x + i] = 1;
+                    }
+                }
+                draw_background(x, y);
+            }
+        }
+
+        public static void delete_line(int x, int y)
+        {
+            for (int k = 1; k <= 10; k++)
+            {
+                line_count = 0;
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    if (background[k, i] == 1)
+                    {
+                        line_count++;
+                    }
+                }
+
+                if (line_count == 10)
+                {
+                    for (int j = k; j > 1; j--)
+                    {
+                        for (int i = 1; i <= 10; i++)
+                        {
+                            background[j, i] = background[j - 1, i];
+                        }
+                    }
+                    draw_background(x, y);
+                }
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo key_value;
+            String ch;
+            int count = 0;
+            int x = 0;
+            int y = 0;
+            
+            draw_background(x,y);
+
+            x = 4; // 블록이 생성되면서 백그라운드 파괴 방지용
+            y = 1;
+
+            Console.SetCursorPosition(x, y);
+            make_block(x,y, now_block);
+            
+            make_block(15, 1, next_block); // 옆에 다음블록 출력만
+
+            while (true)
+            {
+                
+                if (count == 10)
+                {
+                    
+                    overlap_count = overlap_check(x , y + 1, rotate_num);
+                    count = 0;
+
+                    if (overlap_count == 0)
+                    {
+                        delete_block(x, y, now_block);
+                        y++;
+                        make_block(x, y, now_block);
+                        count = 0;
+                    }
+                    else
+                    {
+                        insert_block(x, y); // 블록을 백그라운드에 넣음
+                        delete_line(x, y); // 만약 줄을 다 채우면 줄비우기
+                        
+                        rotate_num = 0; // 로테이트 초기화를 먼저해줘서 딜리트 오류방지
+
+                        delete_block(15, 1, next_block); // 옆에 블록 지우기
+                        now_block = next_block; // 다음블록을 현재블록으로 바꿔줌
+                        next_block = rand.Next(0, 6); // 랜덤함수로 다음블록 만들기
+                        make_block(15, 1, next_block); // 다음블록 출력
+                        
+
+
+                        x = 4;
+                        y = 0;
+                        count = 0;
+                    }
+
+
+                }
+
+
+                if (Console.KeyAvailable)
+                {
+
+                    key_value = Console.ReadKey(true);
+                    ch = key_value.Key.ToString();
+
+                    if (ch == "A")
+                    {
+                        overlap_count = overlap_check(x-1, y, rotate_num);
+                        if(overlap_count == 0)
+                        {
+                            delete_block(x, y, now_block);
+                            x--;
+                            make_block(x, y, now_block);
+                        }
+                    }
+                    else if (ch == "D")
+                    {
+                        overlap_count = overlap_check(x+1, y, rotate_num);
+                        if (overlap_count == 0)
+                        {
+                            delete_block(x, y, now_block);
+                            x++;
+                            make_block(x, y, now_block);
+                        }
+                    }
+                    else if (ch == "W")
+                    {
+                        overlap_count = overlap_check(x, y-1, rotate_num);
+                        if (overlap_count == 0)
+                        {
+                            //delete_block(x, y);
+                            //y--;
+                            //make_block(x, y);
+                        }
+                    }
+                    else if (ch == "S")
+                    {
+                        overlap_count = overlap_check(x, y+1, rotate_num);
+                        if (overlap_count == 0)
+                        {
+                            delete_block(x, y, now_block);
+                            y++;
+                            make_block(x, y, now_block);
+                        }
+                    }
+                    else if (ch == "R")
+                    {
+                        int tmp_rotate_num = rotate_num + 1;
+                        if (tmp_rotate_num == 4)
+                            tmp_rotate_num = 0;
+
+                        overlap_count = overlap_check(x, y, tmp_rotate_num);
+                        if (overlap_count == 0)
+                        {
+                            delete_block(x, y, now_block);
+                            rotate_num++;
+                            if (rotate_num == 4)
+                                rotate_num = 0;
+                            make_block(x, y, now_block);
+                        }
+                    }
+                }
+
+                count++;
+                Thread.Sleep(100);
+                
+            }
+
+
+        }
+    }
+}
