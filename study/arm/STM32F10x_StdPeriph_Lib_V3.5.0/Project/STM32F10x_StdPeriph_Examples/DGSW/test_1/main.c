4a0d7756e56db478486f69a8dba0d5d01062f5b3
riversnails <ekgur120736@gmail.com>
2021-04-14 16:10:12 +0900

    rotary switch

--- study/arm/STM32F10x_StdPeriph_Lib_V3.5.0/Project/STM32F10x_StdPeriph_Examples/DGSW/test_1/main.c
+++ study/arm/STM32F10x_StdPeriph_Lib_V3.5.0/Project/STM32F10x_StdPeriph_Examples/DGSW/test_1/main.c
@@ -2,8 +2,17 @@
 //#include "stm32_eval.h"
 #include <stdio.h>
 
-GPIO_InitTypeDef GPIO_InitStructure;
 
+int fputc(int ch, FILE *f)
+{
+	while(!(USART1->SR & (0x01 << 7)));
+  USART1->DR = (unsigned char)ch;
+  return ch;
+}
+
+void uart_init(USART_InitTypeDef* USART_InitStruct);
+GPIO_InitTypeDef GPIO_InitStructure;
+USART_InitTypeDef USART_InitStructure;
 
 volatile unsigned long sys_count = 0;
 unsigned int TimingDelay = 0;
@@ -38,6 +47,8 @@ void TimingDelay_Decrement(void)
 
 int main(void)
 {
+	unsigned short in_data = 0;
+	char toggle = 0;
 //	SysTick->LOAD = 720;
 //	SysTick->CTRL = 0x07;
 //	
@@ -51,16 +62,17 @@ int main(void)
 //	GPIOE->CRL |= (0x03 << 3 * 4);
 	
 	
-	RCC->APB2ENR |= (0x01 << 2); // GPIOA
-	GPIOA->CRH &= ~(0x0f << 1 * 4); // GPIOA.9
-	GPIOA->CRH |= (0x0b << 1 * 4); // GPIOA.9 : Alternate OUT push/pull
-	GPIOA->CRH &= ~(0x0f << 2 * 4); // GPIOA.10
-	GPIOA->CRH |= (0x04 << 2 * 4); // GPIOA.9 : Floating Input
+//	RCC->APB2ENR |= (0x01 << 2); // GPIOA
+//	GPIOA->CRH &= ~(0x0f << 1 * 4); // GPIOA.9
+//	GPIOA->CRH |= (0x0b << 1 * 4); // GPIOA.9 : Alternate OUT push/pull
+//	GPIOA->CRH &= ~(0x0f << 2 * 4); // GPIOA.10
+//	GPIOA->CRH |= (0x04 << 2 * 4); // GPIOA.9 : Floating Input
+//	
+//	RCC->APB2ENR |= (0x01 << 14); // USART1
+//	USART1->CR1 = (0x01 << 13) | (0x01 << 3) | (0x01 << 2); // UE, TE, RE
+//	USART1->BRR = 0x271;
+// USART1->DR = 0x31;
 	
-	RCC->APB2ENR |= (0x01 << 14); // USART1
-	USART1->CR1 = (0x01 << 13) | (0x01 << 3) | (0x01 << 2); // UE, TE, RE
-	USART1->BRR = 0x271;
-	USART1->DR = 0x31;
 	
 	//-----------------------------
 	//SysTick
@@ -70,15 +82,67 @@ int main(void)
 	}
 	//-----------------------------
 
+	//-----------------------------
+	//USART1
+	USART_InitStructure.USART_BaudRate = 115200;
+  USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+  USART_InitStructure.USART_StopBits = USART_StopBits_1;
+  USART_InitStructure.USART_Parity = USART_Parity_No;
+  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
 
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE, ENABLE);
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_Init(GPIOE, &GPIO_InitStructure);
+	uart_init(&USART_InitStructure);
+	//-----------------------------
+	
+//	//-----------------------------
+//	// GPIOE
+//	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE, ENABLE);
+//	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4;
+//	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+//	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+//	GPIO_Init(GPIOE, &GPIO_InitStructure);
+//	//-----------------------------
+
+	//-----------------------------
+	// GPIOC - rotary
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+	//-----------------------------
+	
+	printf("hello world \r\n\n\n\n\n");
 
 	
-
+	while(1)
+	{
+		in_data = GPIOC->IDR;
+//		if( !(in_data & (0x01 << 6)) && (in_data & (0x01 << 7)) ) toggle++;
+//		else if( !(in_data & (0x01 << 6)) && !(in_data & (0x01 << 7)) ) toggle++;
+//		else if( (in_data & (0x01 << 6)) && !(in_data & (0x01 << 7)) ) toggle++;
+//		else if( !(in_data & (0x01 << 6)) && !(in_data & (0x01 << 7)) ) toggle++;
+//		
+//		if(toggle == 4)
+//		{
+//			toggle = 0;
+//			printf("rotate\r\n");
+//		}
+		
+		
+		if(in_data & (0x01 << 6) && toggle == 0) 
+		{
+			//toggle = 1;
+			printf("high\r\n");
+		}
+		else if( !(in_data & (0x01 << 6) ) && toggle == 0)
+		{			
+			//toggle = 0;
+			printf("low\r\n");
+		}
+		
+		Delay_ms(10);
+	}
+	
 	
 	while(1)
 	{ 
@@ -86,5 +150,26 @@ int main(void)
 		Delay_ms(1000);
 		GPIO_ResetBits(GPIOE, GPIO_Pin_3);
 		Delay_ms(1000);
+		printf("2sec\r\n");
 	}
+}
+
+void uart_init(USART_InitTypeDef* USART_InitStruct)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA , ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  USART_Init(USART1, USART_InitStruct);
+  USART_Cmd(USART1, ENABLE);
 }
